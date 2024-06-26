803ea828dc9a02dd2ce1e559957821668f6d75bb
ArdorHoon <didgnsah7320@gmail.com>
2024-06-26 16:14:25 +0900

    init

---
+++ src/main/kotlin/com/portfolio/hungineer/HungineerApplication.kt
@@ -0,0 +1,11 @@
+package com.portfolio.hungineer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HungineerApplication
+
+fun main(args: Array<String>) {
+    runApplication<HungineerApplication>(*args)
+}
---
+++ src/test/kotlin/com/portfolio/hungineer/HungineerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.portfolio.hungineer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HungineerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
